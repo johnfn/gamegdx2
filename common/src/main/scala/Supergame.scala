1ae8c506227eda83f75e3420aa92deedaaf4233c
johnfn <johnfn@gmail.com>
2014-06-24 08:15:36 -0700

    Map loading.

--- common/src/main/scala/Supergame.scala
+++ common/src/main/scala/Supergame.scala
@@ -8,21 +8,41 @@ import com.badlogic.gdx.graphics.Pixmap.Format
 import com.badlogic.gdx.graphics.TextureData.TextureDataType
 import com.badlogic.gdx.graphics.{Pixmap, TextureData, GL20, Texture}
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.OrthographicCamera
+import com.badlogic.gdx.maps.MapLayer
+import com.badlogic.gdx.maps.tiled.TiledMap
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer
+import com.badlogic.gdx.maps.tiled.TmxMapLoader
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer
+
 
 class Supergame extends Game {
   var batch:SpriteBatch = null
   var img:Texture = null
+  var camera:OrthographicCamera = null
+  var map:TiledMap = null
+  var mapRenderer:TiledMapRenderer = null
 
   override def create() {
+    val width = Gdx.graphics.getWidth()
+    val height = Gdx.graphics.getHeight()
+
     this.batch = new SpriteBatch()
     this.img = new Texture("badlogic.jpg")
+
+    map = new TmxMapLoader().load("map.tmx")
+    mapRenderer = new OrthogonalTiledMapRenderer(map)
+
+    camera = new OrthographicCamera()
+    camera.setToOrtho(true, width, height)
   }
 
   override def render() {
     Gdx.gl.glClearColor(1, 0, 0, 1);
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-    batch.begin();
-      batch.draw(img, 0, 0);
-    batch.end();
+
+    camera.update()
+    mapRenderer.setView(camera)
+    mapRenderer.render()
   }
 }
