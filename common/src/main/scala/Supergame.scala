e4af72917df98063589a004b498840df210b83b5
johnfn <johnfn@gmail.com>
2014-06-24 08:09:59 -0700

    Yay, asset loading works!

--- common/src/main/scala/Supergame.scala
+++ common/src/main/scala/Supergame.scala
@@ -3,6 +3,7 @@ package game
 import com.badlogic.gdx.Game
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.graphics.Pixmap.Format
 import com.badlogic.gdx.graphics.TextureData.TextureDataType
 import com.badlogic.gdx.graphics.{Pixmap, TextureData, GL20, Texture}
@@ -21,7 +22,7 @@ class Supergame extends Game {
     Gdx.gl.glClearColor(1, 0, 0, 1);
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     batch.begin();
-      //batch.draw(img, 0, 0);
+      batch.draw(img, 0, 0);
     batch.end();
   }
 }
