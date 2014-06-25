7640d6f721030a0f8ce2508e9a2d0c4fdb0d8701
johnfn <johnfn@gmail.com>
2014-06-24 22:43:00 -0700

    Start working on tilemap.
    Scala + JRebel = awesomeness. except that it doesn't work for some
    reason

--- common/src/main/scala/Supergame.scala
+++ common/src/main/scala/Supergame.scala
@@ -1,48 +1,110 @@
 package game
 
-import com.badlogic.gdx.Game
-import com.badlogic.gdx.ApplicationAdapter
-import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input.Keys
+import com.badlogic.gdx.maps.objects.RectangleMapObject
+import com.badlogic.gdx.{InputProcessor, Game, ApplicationAdapter, Gdx}
 import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.graphics.Pixmap.Format
 import com.badlogic.gdx.graphics.TextureData.TextureDataType
 import com.badlogic.gdx.graphics.{Pixmap, TextureData, GL20, Texture}
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.OrthographicCamera
-import com.badlogic.gdx.maps.MapLayer
-import com.badlogic.gdx.maps.tiled.TiledMap
-import com.badlogic.gdx.maps.tiled.TiledMapRenderer
-import com.badlogic.gdx.maps.tiled.TmxMapLoader
+import com.badlogic.gdx.maps.{MapLayers, MapLayer}
+import com.badlogic.gdx.maps.tiled.{TiledMapTileLayer, TiledMap, TiledMapRenderer, TmxMapLoader}
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer
+import com.badlogic.gdx.math.{Intersector, Rectangle}
+import scala.collection.JavaConverters._
 
+class Entity(textureFile:String) extends Rectangle {
+  var texture:Texture = new Texture(textureFile)
+}
+
+class Player extends Entity("player.png") {
+  def render(batch:SpriteBatch) {
+    batch.draw(texture, this.x, this.y)
+  }
+
+  def update() {
+    if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
+      this.x += 5
+    }
+
+    if (Gdx.input.isKeyPressed(Keys.LEFT)) {
+      this.x -= 5
+    }
+
+    if (Gdx.input.isKeyPressed(Keys.UP)) {
+      this.y += 5
+    }
+
+    if (Gdx.input.isKeyPressed(Keys.DOWN)) {
+      this.y -= 5
+    }
+  }
+}
+
+class Tilemap(mapName:String) {
+  var map:TiledMap = new TmxMapLoader().load(mapName)
+  var mapRenderer:TiledMapRenderer = new OrthogonalTiledMapRenderer(map)
+
+  def render(camera:OrthographicCamera) {
+    mapRenderer.setView(camera)
+    mapRenderer.render()
+  }
+
+  def layers():Vector[MapLayer] = {
+    Vector() ++ map.getLayers().iterator().asScala
+  }
+
+  def collideWith(entity:Entity):Boolean = {
+    val objects = layers()
+      .map((layer) => Vector() ++ layer.getObjects.asScala)
+      .map((objects) => objects.map((obj) => obj.asInstanceOf[RectangleMapObject].getRectangle())).flatten
+
+    objects.map((rect) => Intersector.overlaps(rect, entity)).contains(true)
+  }
+}
 
 class Supergame extends Game {
   var batch:SpriteBatch = null
   var img:Texture = null
   var camera:OrthographicCamera = null
-  var map:TiledMap = null
-  var mapRenderer:TiledMapRenderer = null
+  var player:Player = null
+  var tilemap:Tilemap = null
 
   override def create() {
-    val width = Gdx.graphics.getWidth()
-    val height = Gdx.graphics.getHeight()
+    val width:Float = Gdx.graphics.getWidth.asInstanceOf[Float]
+    val height:Float = Gdx.graphics.getHeight.asInstanceOf[Float]
 
     this.batch = new SpriteBatch()
     this.img = new Texture("badlogic.jpg")
 
-    map = new TmxMapLoader().load("map.tmx")
-    mapRenderer = new OrthogonalTiledMapRenderer(map)
-
     camera = new OrthographicCamera()
-    camera.setToOrtho(true, width, height)
+    camera.setToOrtho(false, width, height)
+
+    this.tilemap = new Tilemap("map.tmx")
+    this.player = new Player()
   }
 
   override def render() {
-    Gdx.gl.glClearColor(1, 0, 0, 1);
-    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    Gdx.gl.glClearColor(1, 1, 1, 1)
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
+    player.update()
+
+    batch.begin()
+    player.render(batch)
+    batch.end()
 
     camera.update()
-    mapRenderer.setView(camera)
-    mapRenderer.render()
+    tilemap.render(camera)
+
+    println(tilemap.collideWith(player))
+  }
+}
+
+object Supergame {
+  def __rebelReload() = {
+    println("zomg, reload.")
   }
 }
